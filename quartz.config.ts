38841e78b1c9132ca311ff4b4c2840b70acec086
Nishant Khurana <skypawalker@nishantacstudio.lan>
2026-05-12 13:38:32 +0530

    Rename site title to localhost

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Pawhouse",
+    pageTitle: "127.0.0.1",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
