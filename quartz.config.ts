23577585bb7e933f057ff8be9d86cd9a63e98541
Nishant Khurana <skypawalker@nishantacstudio.lan>
2026-05-12 13:39:37 +0530

    Rename site title to Mindscape

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "127.0.0.1",
+    pageTitle: "Mindscape",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
